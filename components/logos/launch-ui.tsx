89eff50da2eed5bea82e30cff12197ca9c5a709e
Llewellyn Y. Fisher <llewellyny.fisher@Llewellyns-MacBook-Air.local>
2025-12-12 23:02:08 -0800

    fix: render Monwhooper logo via SVG image to preserve navbar layout

--- components/logos/launch-ui.tsx
+++ components/logos/launch-ui.tsx
@@ -1,21 +1,24 @@
-import Image from "next/image"
 import * as React from "react"
 
-type LogoProps = {
-  className?: string
-}
-
-const LaunchUI = ({ className }: LogoProps) => {
-  return (
-    <Image
-      src="/monwhooperlogo.png"
-      alt="Monwhooper logo"
-      width={24}
-      height={24}
-      priority
-      className={className}
+const LaunchUI = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <image
+      href="/monwhooperlogo.png"
+      xlinkHref="/monwhooperlogo.png"
+      x="0"
+      y="0"
+      width="24"
+      height="24"
+      preserveAspectRatio="xMidYMid meet"
     />
-  )
-}
+  </svg>
+)
 
 export default LaunchUI
