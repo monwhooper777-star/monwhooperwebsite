997aa3952d546c28ef700bbcdbbc42f9fcbc99b7
Llewellyn Y. Fisher <llewellyny.fisher@Llewellyns-MacBook-Air.local>
2025-12-13 00:00:23 -0800

    fix: render Monwhooper logo with img for maximum compatibility

--- components/logos/launch-ui.tsx
+++ components/logos/launch-ui.tsx
@@ -1,24 +1,35 @@
 import * as React from "react"
 
-const LaunchUI = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    width={24}
-    height={24}
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <image
-      href="/monwhooperlogo.png"
-      xlinkHref="/monwhooperlogo.png"
-      x="0"
-      y="0"
-      width="24"
-      height="24"
-      preserveAspectRatio="xMidYMid meet"
-    />
-  </svg>
-)
+const LaunchUI = (props: React.SVGProps<SVGSVGElement>) => {
+  const size =
+    typeof props.width === "number"
+      ? props.width
+      : typeof props.height === "number"
+        ? props.height
+        : 24
+
+  return (
+    <span
+      className={props.className}
+      style={{
+        display: "inline-block",
+        width: size,
+        height: size,
+        lineHeight: 0,
+        flex: "0 0 auto",
+      }}
+      aria-label="Monwhooper logo"
+      role="img"
+    >
+      <img
+        src="/monwhooperlogo.png"
+        alt="Monwhooper logo"
+        width={Number(size)}
+        height={Number(size)}
+        style={{ width: "100%", height: "100%", display: "block" }}
+      />
+    </span>
+  )
+}
 
 export default LaunchUI
