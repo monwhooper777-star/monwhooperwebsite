1eaa276516a5710e565b2e0845f52834f37b544e
Llewellyn Y. Fisher <llewellyny.fisher@adsl-172-14-80-253.dsl.frs2ca.sbcglobal.net>
2025-12-14 01:35:33 -0800

    Update hero body copy

--- components/sections/hero/default.tsx
+++ components/sections/hero/default.tsx
@@ -32,7 +32,7 @@ interface HeroProps {
 export default function Hero({
   title = "Monwhooper1776 - Stay Dangerous",
   description =
-    "How WE can STAY DANGEROUS in life. DANGEROUS ALONE DEADLIEST WHEN TOGETHER",
+    "Monwhooper1776 - Stay Dangerous is a personal brand created by Kenneth Castenada, the maxim "Stay Dangerous" encapsulates the main philosophy of the brand.",
   mockup = (
     <img
       src="/healingweb.png"
