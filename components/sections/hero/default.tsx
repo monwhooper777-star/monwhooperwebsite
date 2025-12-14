60115a50580e775f5e42ca09880d4bb00800d17c
Llewellyn Y. Fisher <llewellyny.fisher@adsl-172-14-80-253.dsl.frs2ca.sbcglobal.net>
2025-12-14 01:01:36 -0800

    Update hero section copy

--- components/sections/hero/default.tsx
+++ components/sections/hero/default.tsx
@@ -31,7 +31,7 @@ interface HeroProps {
 }
 
 export default function Hero({
-  title = "Give your big idea the design it deserves",
+  title = "Monwhooper1776 - Stay Dangerous",
   description = "Professionally designed blocks and templates built with React, Shadcn/ui and Tailwind that will help your product stand out.",
   mockup = (
     <Screenshot
