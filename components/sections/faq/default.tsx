cf3d3d40bd1ef45cfa990af77082571ec84feafe
Llewellyn Y. Fisher <llewellyny.fisher@Llewellyns-MacBook-Air.local>
2025-12-20 22:10:42 -0800

    Add #qa anchor target to FAQ section

--- components/sections/faq/default.tsx
+++ components/sections/faq/default.tsx
@@ -147,7 +147,7 @@ export default function FAQ({
   className,
 }: FAQProps) {
   return (
-    <Section className={className}>
+    <Section id="qa" className={className}>
       <div className="max-w-container mx-auto flex flex-col items-center gap-8">
         <h2 className="text-center text-3xl font-semibold sm:text-5xl">
           {title}
